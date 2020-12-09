d06de62fc86966a2c83b337c82570889092e934f
Mirko Bertone <bertonemirko@gmail.com>
2020-12-09 16:39:36 +0100

    wip

--- src/controllers/mask.controller.ts
+++ src/controllers/mask.controller.ts
@@ -1,5 +1,8 @@
-import tf from '@tensorflow/tfjs-node-gpu';
+// import tf from '@tensorflow/tfjs-node-gpu';
+import * as tf from '@tensorflow/tfjs-node'
 import * as bodyPix from '@tensorflow-models/body-pix';
+import { createCanvas, loadImage, createImageData, Image, Canvas } from 'canvas'
+import fs from "fs"
 import { NextFunction, Request, Response } from 'express';
 
 class MaskController {
@@ -8,12 +11,7 @@ class MaskController {
         console.log("[getMask]");
         
         try {
-            const net = await bodyPix.load({
-                architecture: 'MobileNetV1',
-                outputStride: 16,
-                multiplier: 0.75,
-                quantBytes: 2,
-            });
+            const net = await bodyPix.load();
             let chunks: any[] = [];
 
             req.on('data', (chunk) => {
@@ -39,6 +37,47 @@ class MaskController {
             next(error);
         }
     }
+
+    // public getMask = async (req: Request, res: Response, next: NextFunction) => {
+
+    //     // const net = await bodyPix.load({
+    //     //     architecture: 'ResNet50',
+    //     //     outputStride: 32,
+    //     //     quantBytes: 2
+    //     // });
+    //     const net = await bodyPix.load();
+    //     console.log("[Net Loaded]");
+        
+    //     const img: any = fs.createWriteStream(__dirname + "/test.jpg")
+    //     let chunks: any[] = [];
+
+    //     req.on('data', (chunk) => {
+    //         chunks.push(chunk);
+    //     });
+
+
+      
+    //     req.pipe(img).on("finish", async () => {
+    //         console.log("[FINISH]");
+
+    //         const i = await loadImage(__dirname + "/test.jpg")
+    //         const canvas = new Canvas(i.width, i.height);
+    //         var ctx = canvas.getContext('2d');
+    //         ctx.drawImage( i, 0, 0);
+    //         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+    //         const image = tf.node.decodeImage()            
+    //         let segmentation = await net.segmentPerson( input, {
+    //             flipHorizontal: false,
+    //             internalResolution: 'medium',
+    //             segmentationThreshold: 0.7,
+    //         });
+    //         res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+    //         res.write(Buffer.from("ciao"))
+    //         res.end();
+    //     });
+    // }
 }
+
 
 export default MaskController;
